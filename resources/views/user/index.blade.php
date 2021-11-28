8fbd7688c444e431d7e26c9b241ea8ccc9e58053
Afif Defriant <defriant17@gmail.com>
2021-11-28 19:30:46 +0700

    update index

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class="top-kategori">
-    <button id="all" class="btn-kategori active"><h5 style="font-size: 16px">All Products</b></h5></button>
+    <button id="all" class="btn-kategori active"><h5 style="font-size: 16px">All</b></h5></button>
     <button id="kitchen-set" class="btn-kategori"><h5 style="font-size: 16px">Kitchen Set</b></h5></button>
     <button id="tempat-tidur" class="btn-kategori"><h5 style="font-size: 16px">Tempat Tidur</b></h5></button>
     <button id="lemari" class="btn-kategori"><h5 style="font-size: 16px">Lemari</b></h5></button>
